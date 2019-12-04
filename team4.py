112931ab0ace97905535c517560863c13f4fdf05
alyson-mccarty <58440792+alyson-mccarty@users.noreply.github.com>
2019-12-04 13:23:42 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -8,7 +8,7 @@
 
 team_name = 'save the turtles sksk' # Only 10 chars displayed.
 strategy_name = 'da snitch most of the time'
-strategy_description = 'betray, betray, collude, betray'
+strategy_description = 'collude every 3rd turn'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
